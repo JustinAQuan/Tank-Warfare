8aa8b8ce1f156523120625f1be713885546ec5b9
Justin Quan <46581109+JustinAQuan@users.noreply.github.com>
2021-04-07 14:24:59 -0700

    added white borders

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -12,8 +12,12 @@ class Play extends Phaser.Scene {
             borderUISize*2,
             0x00FF00,
             ).setOrigin(0,0);
-    }
 
-    // white borders
-    
+        // white borders
+	    
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
+	    this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0 ,0);
+	    this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+	    this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0 ,0);
+    }   
 }

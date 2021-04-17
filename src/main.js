8de0dbe28c390eb2c3b3522c2ecb5370610453d4
Justin Quan <46581109+JustinAQuan@users.noreply.github.com>
2021-04-17 02:31:34 -0700

    added comments to main.js

--- src/main.js
+++ src/main.js
@@ -20,6 +20,7 @@ let keyF, keyA, keyS;
 let keyL, keyLEFT, keyRIGHT;
 
 // ROCKET PATROL MODS USED
+// - Display the time remaining (in seconds) on the screen (10)
 // - Implement a simultaneous two-player mode (30)
 // - Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
 
